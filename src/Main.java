29ca12abaca6bd74a6b4d35af1b8bb6ec6da9250
Phạm Khắc Duy Long <longphamkhacduy@gmail.com>
2024-09-09 19:51:58 +0700

    Merge pull request #2 from vansy204/feature/vigenre
    comment

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
 
+    public static String Caesar(String Plain, int NumKey){
+        String encrypted = "";
+        int n = Plain.length();
+
+        for (int i = 0; i < n; i++) {
+            //ký tự thứ i trong tin gốc
+            char PlainChar = Plain.charAt(i);
+
+            //đẩy lên *NumKey* ký tự
+            PlainChar += NumKey;
+
+            //nếu qua ký tự 'z' thì quay lại 'a'
+            if (PlainChar > 'z')
+                PlainChar = 'a';
+
+            encrypted = encrypted + PlainChar;
+        }
+        return encrypted;
+    }
+
     public static void main(String[] args) {
         int NumKey = 0; //key that are numbers, used in Caesar, and part of permutation
 
@@ -7,6 +27,8 @@ public class Main {
         String Stringkey = "";
 
         //Plain text
-        String Plain = "skibididomdomyesyes ";
+        String Plain = "skibididomdomyesyes";
+
+        System.out.println(Caesar(Plain, 4));
     }
 }
