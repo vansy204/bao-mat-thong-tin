58036fe33aa04b5af97e3d4851ffdb29930e9a03
Phạm Khắc Duy Long <longphamkhacduy@gmail.com>
2024-09-10 20:27:35 +0700

    Rail Fence cipher method

--- src/AutoKey.java
+++ src/AutoKey.java
@@ -17,12 +17,14 @@ public class AutoKey extends Vingenere {
     }
 
     public static void main(String[] args) {
+        //input
         Scanner sc = new Scanner(System.in);
         System.out.print("input plain message: ");
         String Plain = sc.nextLine();
         System.out.print("input key: ");
         String Key = sc.nextLine();
 
+        //output
         Key = GenerateNewKey(Plain, Key);
         System.out.println("Auto generated key: " + Key);
         String encrypted = encrypt(Plain, Key);
